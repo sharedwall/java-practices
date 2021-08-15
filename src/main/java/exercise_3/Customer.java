f0609a2d7d528c1b6aa94ee5bdf178422787329c
Mohamamd Asheri <mhm.asheri@gmail.com>
2021-08-15 08:02:17 +0430

    JAVAP-16 The Author and Book exercise implemented

---
+++ src/main/java/exercise_3/Customer.java
@@ -0,0 +1,49 @@
+package exercise_3;
+
+import java.util.Objects;
+
+public class Customer {
+    private int id;
+    private String name;
+    private char gender;
+
+    public Customer(int id, String name, char gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return getId() == customer.getId() && getGender() == customer.getGender() && getName().equals(customer.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getGender());
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
---
+++ src/main/java/exercise_3/TestAccount.java
@@ -0,0 +1,19 @@
+package exercise_3;
+
+public class TestAccount {
+    public static void main(String[] args) {
+        Customer customer = new Customer(1000, "Mohammad", 'm');
+        System.out.println(customer);
+
+        Account account = new Account(100, customer);
+        account.setBalance(25000);
+        System.out.println(account);
+
+        account.withdraw(11000);
+        System.out.println(account);
+        account.withdraw(32000);
+        account.deposit(18000);
+        account.withdraw(32000);
+        System.out.println(account);
+    }
+}
