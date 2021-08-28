6edd4ec482674a207e93d386e20ec397b3aaac1f
Mohamamd Asheri <mhm.asheri@gmail.com>
2021-08-28 17:28:52 +0430

    JAVAP-30 My first functional interface

--- src/main/java/sprint2/exercise2/ShortToByteFunction.java
+++ src/main/java/sprint2/exercise2/ShortToByteFunction.java
@@ -1,16 +1,6 @@
 package sprint2.exercise2;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-public interface FunctionalInterface {
-    public void execute();
-
-    public default void print(String text) {
-        System.out.println(text);
-    }
-
-    public static void print(String text, PrintWriter writer) throws IOException {
-        writer.write(text);
-    }
+@FunctionalInterface
+public interface ShortToByteFunction {
+    byte applyAsByte(short s);
 }
--- src/main/java/sprint2/exercise2/Test.java
+++ src/main/java/sprint2/exercise2/Test.java
@@ -1,2 +1,20 @@
-package sprint2.exercise2;public class Test {
+package sprint2.exercise2;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        short[] array = {(short) 1, (short) 2, (short) 3};
+        System.out.println(Arrays.toString(array));
+        byte[] transformedArray = transformArray(array, s -> (byte) (s * 2));
+        System.out.println(Arrays.toString(transformedArray));
+    }
+
+    public static byte[] transformArray(short[] array, ShortToByteFunction function) {
+        byte[] transformedArray = new byte[array.length];
+        for (int i = 0; i < array.length; i++) {
+            transformedArray[i] = function.applyAsByte(array[i]);
+        }
+        return transformedArray;
+    }
 }
